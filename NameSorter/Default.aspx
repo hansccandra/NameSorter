34d651bc0a745b29a2935e6435ec82931df46a41
hansc <hansc@HANS-PC>
2021-10-29 22:31:40 +0700

    Add project files.

---
+++ NameSorter/Default.aspx
@@ -0,0 +1,10 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="NameSorter._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="row">
+        <div class="col-md-4">
+            <br />
+            <asp:Button ID="btnSort" runat="server" Text="Sort" OnClick="btnSort_Click" />
+        </div>
+    </div>
+</asp:Content>
---
+++ NameSorter/Default.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace NameSorter
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        public string[] SortName()
+        {
+            string[] names = System.IO.File.ReadAllLines(Server.MapPath(@"~/FileText/unsorted-names-list.txt"));
+            var sortKey = names.Select(SortLast).ToArray();
+            System.Array.Sort(sortKey, names);
+            return names;
+        }
+
+        private string SortLast(string name)
+        {
+            string[] names = name.Split(' ');
+            var length = names.Length;
+
+            return names[length - 1] + ' ' + string.Join(' '.ToString(), names, 0, length - 1);
+        }
+
+        protected void btnSort_Click(object sender, EventArgs e)
+        {
+            var names = SortName();
+            System.IO.File.WriteAllLines(Server.MapPath(@"~/FileText/sorted-names-list.txt"), names);
+        }
+    }
+}
+
